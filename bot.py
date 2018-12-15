54e94460c0a3b8d4bda5d37351e48a8765a8e005
Ondo <8308572+NeoMahler@users.noreply.github.com>
2018-12-15 17:22:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,6 +151,8 @@ async def identity(ctx, *, arg):
     for file in images_list:
         if article in file['title']:
             flag_name = file['title']
+    if flag_name == None:
+        flag_name = "File:Wikilogo new.png" # This will have to be replaced for the actual identity flag
     # Get flag url
     flag_link = requests.get(url="https://nonbinary.wiki/w/api.php?action=query&titles={0}&prop=imageinfo&iiprop=url&format=json".format(flag_name))
     flag = next(iter(flag_link.json()['query']['pages'].values()))['imageinfo'][0]['url']
