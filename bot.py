b772bb5c5f751670dd2577709024852acf6ad7aa
Ondo <8308572+NeoMahler@users.noreply.github.com>
2019-12-06 11:52:46 +0100

    fix syntax error

--- bot.py
+++ bot.py
@@ -361,7 +361,7 @@ async def pronoun(ctx, arg = None):
     embed.add_field(name="Objective", value="I like **{}**.".format(obj), inline=True)
     embed.add_field(name="Possessive Determiner", value="**{}** smile is pretty.".format(posad), inline=True)
     embed.add_field(name="Possessive Pronoun", value="The book is **{}**.".format(pos), inline=True)
-    embed.add_field(name="Reflexive", value="{} did by **{}**.",format(sub.capitalize(), ref), inline=True)
+    embed.add_field(name="Reflexive", value="{} did by **{}**.".format(sub.capitalize(), ref), inline=True)
     embed.add_field(name="Frequency", value=freq, inline=True)
     embed.set_footer(text="Remember! If you are not sure, just ask!")
 
