37430a4e5cbdd0c9b19380869d8e9cb9eb4a9ac1
Ondo <8308572+NeoMahler@users.noreply.github.com>
2021-01-12 20:37:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import logging, sys, traceback, os
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
