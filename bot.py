ca644281a36656e8af296a059125cc4ffcdd5e1f
Ondo <8308572+NeoMahler@users.noreply.github.com>
2021-01-19 12:15:16 +0100

    add nb! prefix alias

--- bot.py
+++ bot.py
@@ -15,12 +15,12 @@ def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
     # Notice how you can use spaces in prefixes. Try to keep them simple though.
-    prefixes = ['!']
+    prefixes = ['!', 'nb!']
 
     # Check to see if we are outside of a guild. e.g DM's etc.
     if not message.guild:
         # Only allow ? to be used in DMs
-        return ['!', '']
+        return ['!', 'nb!', '']
 
     # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
     return commands.when_mentioned_or(*prefixes)(bot, message)
