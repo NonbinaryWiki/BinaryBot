99c1cbfcfdf7b7e4699038a9b3d3ee2d33e1a914
Roc <roc@worldanvil.com>
2022-04-13 19:38:28 +0200

    Working on #44

--- modules/old-commands.py
+++ modules/old-commands.py
@@ -7,27 +7,27 @@ class OldCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
+    @commands.command(name="identity")
     async def identity(self, ctx):
         await ctx.send(msg)
 
-    @commands.command()
+    @commands.command(name="pronoun")
     async def pronoun(self, ctx):
         await ctx.send(msg)
 
-    @commands.command()
+    @commands.command(name="pronountest")
     async def pronountest(self, ctx):
         await ctx.send(msg)
 
-    @commands.command()
+    @commands.command(name="ping")
     async def ping(self, ctx):
         await ctx.send(msg)
 
-    @commands.command()
+    @commands.command(name="help")
     async def help(self, ctx):
         await ctx.send("I've migrated to slash commands due to new limitations implemented by Discord. To see all of my commands, **type `/` and click on my logo.**")
 
-    @commands.command()
+    @commands.command(name="flag")
     async def flag(self, ctx):
         await ctx.send(msg)
 
