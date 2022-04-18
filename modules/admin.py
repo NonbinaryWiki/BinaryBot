6103f6bca6f8d711526742e646bcb340be29a94e
Roc <roc@worldanvil.com>
2022-04-18 11:04:24 +0200

    fixing owner-only commands

--- modules/admin.py
+++ modules/admin.py
@@ -6,9 +6,8 @@ import subprocess
 class AdminCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.admin_guilds = [551837071703146506]
     
-    @slash_command(name="update", description="Pulls the latest version from GitHub (owner-only).", guild_ids=self.admin_guilds)
+    @slash_command(name="update", description="Pulls the latest version from GitHub (owner-only).", guild_ids=[551837071703146506])
     @permissions.is_owner()
     async def update(self, ctx):
         await ctx.defer()
@@ -16,7 +15,7 @@ class AdminCog(commands.Cog):
         #output = subprocess.check_output("git pull", shell=True)
         await ctx.respond("Pulling latest version from GitHub: ```" + str(output) + "``` Remember to use the reload command for the changes to take effect.")
 
-    @slash_command(name="load", description="Loads a previously unloaded module (owner-only).", guild_ids=self.admin_guilds)
+    @slash_command(name="load", description="Loads a previously unloaded module (owner-only).", guild_ids=[551837071703146506])
     @permissions.is_owner()
     async def load(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
@@ -27,7 +26,7 @@ class AdminCog(commands.Cog):
         else:
             await ctx.respond('Module loaded! :tada:')
 
-    @slash_command(name="unload", description="Unloads a previously loaded module (owner-only).", guild_ids=self.admin_guilds)
+    @slash_command(name="unload", description="Unloads a previously loaded module (owner-only).", guild_ids=[551837071703146506])
     @permissions.is_owner()
     async def unload(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
@@ -38,7 +37,7 @@ class AdminCog(commands.Cog):
         else:
             await ctx.respond('Module unloaded! :tada:')
 
-    @slash_command(name="reload", description="Reloads a previously loaded module (owner-only).", guild_ids=self.admin_guilds)
+    @slash_command(name="reload", description="Reloads a previously loaded module (owner-only).", guild_ids=[551837071703146506])
     @permissions.is_owner()
     async def reload(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
@@ -50,7 +49,7 @@ class AdminCog(commands.Cog):
         else:
             await ctx.respond('Module reloaded! :tada:')
 
-    @slash_command(name="servers", description="Displays a list of all servers the bot is in (owner-only).", guild_ids=self.admin_guilds)
+    @slash_command(name="servers", description="Displays a list of all servers the bot is in (owner-only).", guild_ids=[551837071703146506])
     @permissions.is_owner()
     async def servers(self, ctx):
         activeservers = self.bot.guilds
@@ -59,7 +58,7 @@ class AdminCog(commands.Cog):
             servernames.append(guild.name)
         await ctx.respond("I'm in **{0}** servers:  {1}".format(len(servernames), ", ".join(servernames)))
 
-    @slash_command(name="error", description="Forcibly causes the bot to error, for debugging purposes (owner-only).", guild_ids=self.admin_guilds)
+    @slash_command(name="error", description="Forcibly causes the bot to error, for debugging purposes (owner-only).", guild_ids=[551837071703146506])
     @permissions.is_owner()
     async def error(self, ctx):
         raise ValueError("Someone used !error.")
