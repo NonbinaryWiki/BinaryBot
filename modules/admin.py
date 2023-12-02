84acc3b89a97de2b3f3869b24deebb177c221123
NeoMahler <roc.humet@gmail.com>
2023-12-02 18:51:55 +0900

    Update to pycord 2.4

--- modules/admin.py
+++ modules/admin.py
@@ -1,6 +1,5 @@
-import discord
 from discord.ext import commands
-from discord.commands import slash_command, permissions, Option
+from discord.commands import slash_command, Option
 import subprocess
 
 class AdminCog(commands.Cog):
@@ -8,14 +7,14 @@ class AdminCog(commands.Cog):
         self.bot = bot
     
     @slash_command(name="update", description="Pulls the latest version from GitHub (owner-only).", guild_ids=[551837071703146506])
-    @permissions.is_owner()
+    @commands.is_owner()
     async def update(self, ctx):
         await ctx.defer()
         output = subprocess.check_output("git pull", shell=True)
         await ctx.respond("Pulling latest version from GitHub: ```" + str(output) + "``` Remember to use the reload command for the changes to take effect.")
 
     @slash_command(name="load", description="Loads a previously unloaded module (owner-only).", guild_ids=[551837071703146506])
-    @permissions.is_owner()
+    @commands.is_owner()
     async def load(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
         try:
@@ -26,7 +25,7 @@ class AdminCog(commands.Cog):
             await ctx.respond('Module loaded! :tada:')
 
     @slash_command(name="unload", description="Unloads a previously loaded module (owner-only).", guild_ids=[551837071703146506])
-    @permissions.is_owner()
+    @commands.is_owner()
     async def unload(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
         try:
@@ -37,7 +36,7 @@ class AdminCog(commands.Cog):
             await ctx.respond('Module unloaded! :tada:')
 
     @slash_command(name="reload", description="Reloads a previously loaded module (owner-only).", guild_ids=[551837071703146506])
-    @permissions.is_owner()
+    @commands.is_owner()
     async def reload(self, ctx, cog: Option(str, "Name of the module (without .py extension)")):
         module = "modules." + cog
         try:
@@ -49,7 +48,7 @@ class AdminCog(commands.Cog):
             await ctx.respond('Module reloaded! :tada:')
 
     @slash_command(name="servers", description="Displays a list of all servers the bot is in (owner-only).", guild_ids=[551837071703146506])
-    @permissions.is_owner()
+    @commands.is_owner()
     async def servers(self, ctx):
         activeservers = self.bot.guilds
         servernames = []
@@ -58,7 +57,7 @@ class AdminCog(commands.Cog):
         await ctx.respond("I'm in **{0}** servers:  {1}".format(len(servernames), ", ".join(servernames)))
 
     @slash_command(name="error", description="Forcibly causes the bot to error, for debugging purposes (owner-only).", guild_ids=[551837071703146506])
-    @permissions.is_owner()
+    @commands.is_owner()
     async def error(self, ctx):
         raise ValueError("Someone used !error.")
         await ctx.respond("Don't you DARE give me an error you dunce! :(")
